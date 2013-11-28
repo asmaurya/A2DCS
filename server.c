5ccc03923799745f8fc9ccb2d36f972d064065c6
asmaurya <asmaurya.in@gmail.com>
2013-11-28 23:53:25 +0530

    Implemeted case 2

--- server.c
+++ server.c
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <netinet/in.h>
 
 //define colors
 #define ANSI_RED "\x1b[31m"
@@ -12,9 +13,15 @@
 #define ANSI_BLUE "\x1b[36m"
 #define ANSI_DEFAULT "\x1b[0m"
 
-int func_speak(char*, int); // It goes out and connects to the listening application
+int func_bind(int); // It goes out and connects to the listening application
 
 int func_listen(int);	// It listens for incoming connections
+
+int func_accept(int);
+
+int func_connect(int, char*, int);
+
+void func_close_socket(int);  //closes socket
 
 void print_color_text(char*, char, int, int);
 
@@ -23,14 +30,14 @@ void print_error(char*);	//prints error in red
 int slow_down_interface(char *, int); //slow down the interface by the specified seconds
 
 int main(int argc, char **argv) {
-	int local_port, remote_port, choice = 1;
-	//char* read_me;
-	char* remote_ip;
+	int local_port, remote_port, choice = 1, socket_connection, socket_handle;
+	char buffer[2] = { 0 };
+	char remote_ip[15]={0};
 	char* interface = "lo";
 
 	//slow_down_interface("lo", 2);
 	//slow the network
-	slow_down_interface(interface, 2);
+//	slow_down_interface(interface, 2);
 	//PRINT THE HEADING
 	printf(ANSI_YELLOW);
 	print_color_text(
@@ -47,7 +54,7 @@ int main(int argc, char **argv) {
 	print_color_text("", 'D', 1, 0);
 
 	//slow the network
-	slow_down_interface(interface, 2);
+	//slow_down_interface(interface, 2);
 
 	//set the socket handle
 
@@ -62,9 +69,78 @@ int main(int argc, char **argv) {
 		switch (choice) {
 
 			case 1:		//Normal Open
-				func_listen(local_port);
+				socket_handle = func_bind(local_port);
+				if (func_listen(socket_handle) > 0) {
+
+					if ((socket_connection = func_accept(socket_handle)) > 0) {
+						while (socket_connection > 0) {
+							if (read(socket_connection, buffer, 1) < 0) {
+								close(socket_handle);
+								print_error(
+										"Could not read incoming connections. ERROR:- ");
+								return (0);
+							}
+
+							//display message and return the same to client
+							print_color_text("Received: ", 'D', 0, 0);
+							print_color_text(buffer, 'Y', 0, 1);
+							print_color_text("Sending: ", 'D', 0, 0);
+							print_color_text(buffer, 'Y', 0, 1);
+							print_color_text("", 'D', 0, 2);
+
+							//if(buffer == 'Q')
+
+							if (write(socket_connection, buffer, 1) < 0) {
+								close(socket_handle);
+								print_error(
+										"Could not send message to the client. ERROR:- ");
+								return (0);
+							}
+
+							//detect if command to quit the connection has been called
+							if (buffer[0] == 'C') break;
+
+						}
+						print_color_text("Closing Connection..", 'R', 0, 1);
+						close(socket_connection);
+					}
+				} else {
+					print_error("Please try again..");
+				}
+				func_close_socket(socket_handle);
 				break;
 			case 2:		//simultaneous open
+				socket_handle = -1;
+				bzero((char*) &buffer, sizeof(buffer));
+				strcat(buffer, "A");
+				if ((socket_handle = func_bind(local_port)) >= 0) {
+					if (func_connect(socket_handle, remote_ip, remote_port) > 0) {
+
+						if (recv(socket_handle, buffer, 1, 0) < 0) {
+							print_error("Receive failed. ERROR:- ");
+							close(socket_handle);
+							continue;
+						} else {
+							print_color_text("Received: ", 'D', 0, 0);
+							print_color_text(buffer, 'Y', 0, 1);
+						}
+
+						if (send(socket_handle, buffer, 1, 0) < 0) {
+							print_error("Send failed. ERROR:- ");
+							close(socket_handle);
+							continue;
+						} else {
+							print_color_text("Send: ", 'D', 0, 0);
+							print_color_text(buffer, 'Y', 0, 1);
+						}
+					}
+
+				} else {
+					print_error("Please try again..");
+				}
+
+				func_close_socket(socket_handle);
+
 				break;
 			case 3:		//active close by server
 				break;
@@ -109,6 +185,9 @@ void print_color_text(char *message, char color, int underline,
 			break;
 		case 'B':
 			printf(ANSI_BLUE);
+			break;
+		case 'D':
+			printf(ANSI_DEFAULT);
 			break;
 		default:
 			printf(ANSI_DEFAULT);
@@ -180,17 +259,17 @@ int slow_down_interface(char * interface, int secs) {
 
 }
 
-int func_listen(int local_port) {
-	int socket_handle, socket_connection;
+int func_bind(int local_port) {
+	int socket_handle;
 	struct sockaddr_in listen_addr;	//, client_addr;
-	char buffer[2] = { 0 }; //buffer to receive and send messages
 
 //create socket
 	if ((socket_handle = socket(AF_INET, SOCK_STREAM, IPPROTO_IP)) < 0) {
 		print_error("Cannot open socket to listen. ERROR:- ");
 		close(socket_handle);
-		return (0);
-	} /* if (sockethandle) */
+		return (-1);
+	}
+
 
 //initialize the struct to zero
 	bzero((char*) &listen_addr, sizeof(listen_addr));
@@ -205,54 +284,55 @@ int func_listen(int local_port) {
 			sizeof(listen_addr)) < 0) {
 		close(socket_handle);
 		print_error("Cannot bind server to the specified socket. ERROR:- ");
-		return (0);
+		return (-1);
 	}
+	print_color_text("Socket binded to port - ", 'D',0,0);
+	printf(ANSI_GREEN);
+	printf("%d\n", local_port);
+	printf(ANSI_DEFAULT);
+	return (socket_handle);
 
+}
+
+int func_listen(int socket_handle) {
 //listen for incoming connection
-	listen(socket_handle, 5);
-	print_color_text("Waiting for incoming connection..", 'D', 0, 1);
+	if (listen(socket_handle, 5) < 0) {
+		print_error("Could not listen on the socket. ERROR:- ");
+		return (0);
+	} else {
+		print_color_text("Waiting for incoming connection..", 'D', 0, 1);
+		return (1);
+	}
+}
 
-//	if ((socket_connection = accept(socket_handle,
-//			(struct sockaddr *) &client_addr,
-//			(socklen_t*) (sizeof(struct sockaddr_in)))) > 0)
-	if ((socket_connection = accept(socket_handle, NULL, NULL)) > -1)
-		print_color_text("Client Connected to server", 'G', 0, 2);
-	else {
-		print_error("Could not accept incoming connection. ERROR:- ");
+int func_connect(int socket_handle, char* remote_ip, int remote_port) {
+	struct sockaddr_in remote_addr;
+	bzero((char*) &remote_addr, sizeof(remote_addr));
+	remote_addr.sin_family = AF_INET;
+	remote_addr.sin_addr.s_addr = inet_addr(remote_ip);
+	remote_addr.sin_port = htons(remote_port);
+	if (connect(socket_handle, (struct sockaddr*) &remote_addr,
+			sizeof(remote_addr)) < 0) {
+		print_error("Could not connect to the remote machine. ERROR:- ");
 		return (0);
 	}
-
-	while (socket_connection > 0) {
-
-		if (read(socket_connection, buffer, 1) < 0) {
-			close(socket_handle);
-			print_error("Could not read incoming connections. ERROR:- ");
-			return (0);
-		}
-
-		//display message and return the same to client
-		print_color_text("Received: ", 'D', 0, 0);
-		print_color_text(buffer, 'Y', 0, 1);
-		print_color_text("Sending: ", 'D', 0, 0);
-		print_color_text(buffer, 'Y', 0, 1);
-		print_color_text("", 'D', 0, 2);
-
-		//if(buffer == 'Q')
-
-		if (write(socket_connection, buffer, 1) < 0) {
-			close(socket_handle);
-			print_error("Could not send message to the client. ERROR:- ");
-			return (0);
-		}
-
-		//detect if command to quit the connection has been called
-		if (buffer[0] == 'C') break;
-
-	}
-	print_color_text("Closing Connection..", 'R', 0, 1);
-	close(socket_connection);
-	close(socket_handle);
+	print_color_text("Connected to the remote machine..", 'G', 0,1);
 	return (1);
+}
+
+int func_accept(int socket_handle) {
+	int socket_connection;
+	if ((socket_connection = accept(socket_handle, NULL, NULL)) > -1) {
+		print_color_text("Client Connected to server..", 'G', 0, 2);
+		return (socket_connection);
+	} else {
+		print_error("Could not accept incoming connection. ERROR:- ");
+		return (-1);
+	}
+}
+void func_close_socket(int socket_handle) {
+	close(socket_handle);
+	print_color_text("Closing connection..", 'R', 0, 1);
 }
 
 void print_error(char* err_msg) {
@@ -260,5 +340,4 @@ void print_error(char* err_msg) {
 	perror(err_msg);
 	printf(ANSI_DEFAULT);
 	printf("\n");
-
 }
